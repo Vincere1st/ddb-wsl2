031baa5e22acffe30bb71d77505216a5808c85c0
Vincere1st <vincerecaro@gmail.com>
2021-12-13 23:28:14 +0100

    add sleep at start

--- 00-wsl2-systemd.sh
+++ 00-wsl2-systemd.sh
@@ -2,7 +2,11 @@ SYSTEMD_PID=$(ps -efw | grep '/lib/systemd/systemd --system-unit=basic.target$' 
 
 if [ -z "$SYSTEMD_PID" ]; then
    sudo /usr/bin/daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
+   while [ -z "$SYSTEMD_PID" ]; do
+        echo "Sleeping for 1 second to let systemd settle"
+        sleep 1
    SYSTEMD_PID=$(ps -efw | grep '/lib/systemd/systemd --system-unit=basic.target$' | grep -v unshare | awk '{print $2}')
+   done
 fi
 
 if [ -n "$SYSTEMD_PID" ] && [ "$SYSTEMD_PID" != "1" ]; then
